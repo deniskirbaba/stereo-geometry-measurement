4350a5b9443574555bfe31e38280ef49c5a43b70
deniskirbaba <kirbabadenis@gmail.com>
2024-09-07 15:23:35 +0300

    feat(detector): add orb, sift, superpoint methodscomparson b/w methods are in ipynb file

--- src/feature_detection/orb_sift_detectors.py
+++ src/feature_detection/orb_sift_detectors.py
@@ -1,18 +1,20 @@
 import cv2
 
-def extract_keypoints_and_descriptors(image_path_1, image_path_2, detector_type='ORB'):
+
+def extract_keypoints_and_descriptors(image_path_1, image_path_2, detector_type="ORB"):
     """
     Extracts keypoints and descriptors from two images.
-    
+
     Args:
         image_path_1 (str): Path to the first image (left image from stereo camera).
         image_path_2 (str): Path to the second image (right image from stereo camera).
         detector_type (str): Feature detector to use ('ORB', 'SIFT'). Default is 'ORB'.
-    
+
     Returns:
         tuple: A tuple containing keypoints and descriptors for both images:
             - keypoints_1, descriptors_1: Keypoints and descriptors of the first image.
             - keypoints_2, descriptors_2: Keypoints and descriptors of the second image.
+
     """
 
     img1 = cv2.imread(image_path_1, cv2.IMREAD_GRAYSCALE)
@@ -21,9 +23,9 @@ def extract_keypoints_and_descriptors(image_path_1, image_path_2, detector_type=
     if img1 is None or img2 is None:
         raise FileNotFoundError("One or both image paths are invalid or images cannot be read.")
 
-    if detector_type == 'ORB':
+    if detector_type == "ORB":
         detector = cv2.ORB_create(nfeatures=1000, nlevels=16)
-    elif detector_type == 'SIFT':
+    elif detector_type == "SIFT":
         detector = cv2.SIFT_create()
     else:
         raise ValueError(f"Unsupported detector type: {detector_type}. Use 'ORB', 'SIFT', or 'SURF'.")
@@ -35,19 +37,15 @@ def extract_keypoints_and_descriptors(image_path_1, image_path_2, detector_type=
 
 
 if __name__ == "__main__":
-    test_imgs_path = 'test_imgs/'
-    image_path_1 = test_imgs_path + 'cam1_1.jpg'
-    image_path_2 = test_imgs_path + 'cam2_1.jpg'
+    TEST_IMGS_PATH = "test_imgs/"
+    IMG_PATH_1 = TEST_IMGS_PATH + "cam1_1.jpg"
+    IMG_PATH_2 = TEST_IMGS_PATH + "cam2_1.jpg"
 
     try:
-        (keypoints_1, descriptors_1), (keypoints_2, descriptors_2) = extract_keypoints_and_descriptors(image_path_1, 
-                                                                                                       image_path_2, 
-                                                                                                       detector_type='ORB')
+        (kp1, d1), (kp2, d2) = extract_keypoints_and_descriptors(IMG_PATH_1, IMG_PATH_2, detector_type="ORB")
 
-        print(f"Keypoints in image 1: {len(keypoints_1)}")
-        print(f"Keypoints in image 2: {len(keypoints_2)}")
+        print(f"Keypoints in image 1: {len(kp1)}")
+        print(f"Keypoints in image 2: {len(kp2)}")
 
     except FileNotFoundError as e:
         print(e)
-    except Exception as e:
-        print(f"An error occurred: {e}")
